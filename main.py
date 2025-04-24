5b0fc28ec54f07e2c232d627b79ae193725edb92
itsutki <utkugaspak@gmail.com>
2025-04-24 22:53:36 +0200

    Add PUT, DELETE, GET by ID endpoints

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Depends, HTTPException
-from models.post import TodoCreate
+from models.post import TodoCreate, TodoUpdate
 from models.response import TodoResponse
 from database import TodoDB, SessionLocal
 from typing import List
@@ -27,6 +27,17 @@ async def read_todos(db: Session = Depends(get_db)): # dif. sessions for every r
     todo_item = db.query(TodoDB).all()
     return todo_item
 
+@app.put("/{todo_id}", response_model=TodoResponse)
+async def update_todo(todo_id:int, item:TodoUpdate, db: Session = Depends(get_db)):
+    todo_item = db.query(TodoDB).filter(TodoDB.id == todo_id).first()
+    if not todo_item:
+        raise HTTPException(status_code=404, detail="Todo ID not found")
+    todo_item.title = item.title
+    todo_item.amount = item.amount
+    db.commit()
+    db.refresh(todo_item)
+    return todo_item
+
 @app.post("/", response_model=TodoResponse)
 async def create_item(item: TodoCreate, db: Session = Depends(get_db)): #Todo: post req. model
     item = TodoDB(title = item.title, amount = item.amount) #Todos db model
@@ -34,3 +45,12 @@ async def create_item(item: TodoCreate, db: Session = Depends(get_db)): #Todo: p
     db.commit()
     db.refresh(item) #  Always refresh → best practice.
     return item
+
+@app.delete("/{todo_id}")
+async def delete_item(todo_id: int, db: Session = Depends(get_db)):
+    item = db.query(TodoDB).filter(TodoDB.id == todo_id).first()
+    if not item:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    db.delete(item)
+    db.commit()
+    return {"message": f"Todo number {todo_id} is deleted successfully."}
--- models/post.py
+++ models/post.py
@@ -1,8 +1,16 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class TodoCreate(BaseModel):
     title: str
     amount: int
+
+    class Config:
+        from_attributes=True
+
+class TodoUpdate(BaseModel):
+    title: Optional[str]
+    amount: Optional[int]
 
     class Config:
         from_attributes=True
